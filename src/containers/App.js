8f2646c6e302a631259e4b73710d8529d175f840
Andrei Zhaleznichenka <Andrei.Zhaleznichenka@helmes.ee>
2018-01-22 15:16:39 +0300

    Add demo support for roles on UI

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import { Panel, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
+import jwtDecode from 'jwt-decode';
 
 import Header from '../components/Header/Header';
 import ManageEmployee from '../components/ManageEmployee/ManageEmployee';
@@ -17,6 +18,13 @@ import { logout } from '../functions/loginFunctions';
 }))
 class App extends Component {
   render() {
+    const roles = jwtDecode(localStorage.token).roles;
+    const stories = {
+      ADMIN: ['can crud on users', 'can assign user roles'],
+      OFFICE: ['can crud on employees', 'can view vacations', 'can grab reports'],
+      MANAGER: ['can view employees', 'can view vacations', 'can approve/reject vacations', 'can grab reports'],
+      EMPLOYEE: ['can crud on their vacations', 'can change their password']
+    }
     return (
       <div className="App">
         <Panel>
@@ -41,6 +49,10 @@ class App extends Component {
         <Button style={{ marginRight: '5px' }} onClick={logout}>logout</Button>
         <Button style={{ marginRight: '5px' }} onClick={() => this.props.i18n.changeLanguage('ru')}>ru</Button>
         <Button style={{ marginRight: '5px' }} onClick={() => this.props.i18n.changeLanguage('en')}>en</Button>
+        Roles: { roles.map(r => <span key={r}>{r} </span>) }<br/><br/>
+        Stories: { roles.map(r => {
+          return stories[r].map((s, si) => <div key={r + si}>{s}</div>);
+        })}
       </div>
     );
   }
